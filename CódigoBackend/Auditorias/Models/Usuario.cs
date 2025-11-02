fed4174efadc42701f21bdef18cc59a84bd46a77
Trianakare27-eng <trianakaren27@gmail.com>
2025-11-02 01:47:11 -0500

    update entity

--- CódigoBackend/Auditorias/Models/Usuario.cs
+++ CódigoBackend/Auditorias/Models/Usuario.cs
@@ -9,31 +9,42 @@ namespace Auditorias.Models
     public class Usuario
     {
         [Key]
+        [Column("id")]
         public Guid Id { get; set; } = Guid.NewGuid();
 
         [Column("nombres")]
         public string Nombres { get; set; }
+
         [Column("apellidos")]
         public string Apellidos { get; set; }
+
         [Column("id_tipo_documento")]
         public Guid IdTipoDocumento { get; set; }
+
+        [ForeignKey(nameof(IdTipoDocumento))]
+        public TipoDocumento TipoDocumento { get; set; }
+
         [Column("documento")]
         public string Documento { get; set; }
+
         [Column("tipo_usuario")]
         public string TipoUsuario { get; set; }
+
         [Column("email")]
         public string Email { get; set; }
+
         [Column("direccion")]
         public string Direccion { get; set; }
+
         [Column("contraseña")]
         public string Contraseña { get; set; }
+
         [Column("fecha_creado")]
         public DateTime FechaCreado { get; set; } = DateTime.Now;
+
         [Column("fecha_actualizado")]
         public DateTime? FechaActualizado { get; set; }
 
-        // Relaciones
-        public TipoDocumento TipoDocumento { get; set; }
         public ICollection<UsuarioRol> UsuarioRoles { get; set; }
         public ICollection<LogSistema> Logs { get; set; }
         public ICollection<Dispositivo> Dispositivos { get; set; }
